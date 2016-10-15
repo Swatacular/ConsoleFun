4219d13e0a29e7084302a53bdab402c023cdb9d8
Thomas Justin Krueger <Tomjk@acdmail.com>
2016-10-15 01:39:16 -0400

    Finish Homework Program

--- 001/ConsoleFun001/Program.cs
+++ 001/ConsoleFun001/Program.cs
@@ -10,6 +10,20 @@ namespace ConsoleFun001
     {
         static void Main(string[] args)
         {
+            string input = Console.ReadLine();
+            List<char> inputList = new List<char>();
+            for (int i = 0; i < (input.Count<char>()); i++)
+            {
+                inputList.Add((input.ElementAt(i)));
+            }
+            Console.WriteLine("Reversed Input");
+            foreach (char charcter in inputList.Reverse<char>())
+            {
+                Console.Write(charcter);
+            }
+            Console.WriteLine();
+            Console.Write("Press any key to end program...");
+            Console.ReadKey();
         }
     }
 }
